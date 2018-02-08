74cb8ec56eee2d790c51880ab7bd170510c2f331
Todd Radin <todd.radin@gmail.com>
2018-02-08 11:44:47 -0600

    Initial Commit

--- BlurDetectionROI/Form1.Designer.cs
+++ BlurDetectionROI/Form1.Designer.cs
@@ -28,12 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1600, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.openToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(140, 30);
+            this.openToolStripMenuItem1.Text = "Open";
+            this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(140, 30);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(271, 801);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 129);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Show Edges";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.showEdgesButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(42, 963);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(405, 183);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(42, 801);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 129);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Show Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.showColorButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(79, 331);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(300, 32);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Please make selection";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(373, 40);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Variance of Laplacian:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(79, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Please make selection";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Recommendation:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(13, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(502, 1184);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(539, 37);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1044, 1184);
+            this.panel2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1044, 1184);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1600, 1233);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Blur Detection";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- BlurDetectionROI/Form1.cs
+++ BlurDetectionROI/Form1.cs
@@ -1,20 +1,298 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
 
 namespace BlurDetectionROI
 {
     public partial class Form1 : Form
     {
+        private Image<Bgr, Byte> _img;
+        private Image<Bgr, Byte> _roi;
+        private (int top, int right, int bottom, int left) _imgBounds;
+        private Mat _lap;
+        private Point _startPos;
+        private Point _endPos;
+        private bool _drawing;
+        private Rectangle _rectangle;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    _img = new Image<Bgr, Byte>(ofd.FileName);
+                    _roi = new Image<Bgr, Byte>(ofd.FileName);
+                    pictureBox1.Image = _img.Bitmap;
+                    _imgBounds = GetPictureBoxImageBounds(pictureBox1);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (_img == null) return;
+            _startPos = e.Location;
+            _drawing = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_img == null) return;
+            _endPos = e.Location;
+            if (_drawing) pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (_img == null) return;
+            if (_drawing)
+            {
+                _drawing = false;
+                var rect = GetRectangle(_startPos, _endPos);
+                if (rect.Width > 0 && rect.Height > 0) _rectangle = rect;
+                pictureBox1.Invalidate();
+
+                // Compute sharpness value from selected region and display 
+                // recommendations to user
+                SetROI();
+                var variance = VarianceOfLaplacian();
+                DisplayResults(variance);
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (_drawing) e.Graphics.DrawRectangle(Pens.Red, GetRectangle(_startPos, _endPos));
+        }
+
+        private void SetROI()
+        {
+            if (ValidateCoordinates())
+            {
+                // Convert coordinates from pictureBox to the original image
+                var start = ConvertCoordinates(pictureBox1, _startPos);
+                var end = ConvertCoordinates(pictureBox1, _endPos);
+
+                // Set region of interest and pictureBox to new ROI
+                _roi.ROI = GetRectangle(start, end);
+                pictureBox1.Image = _roi.Bitmap; 
+            }
+            else
+            {
+                MessageBox.Show("Selection is out of bounds. Please make selection within image.");
+            }
+        }
+
+        /// <summary>
+        /// Computes degree of sharpness by using laplacian edge detection.
+        /// </summary>
+        /// <returns>The variance of the laplacian operator.</returns>
+        private double VarianceOfLaplacian()
+        {
+            // Preprocess image brightness by equalizing histogram
+            var equalizedHistogram = EqualizeHistogram(_roi.Mat);
+
+            // Convert image to grayscale
+            var gray = new Mat();
+            CvInvoke.CvtColor(equalizedHistogram, gray, ColorConversion.Bgr2Gray);
+
+            // Bilateral filter to reduce noise of high ISO images
+            var bilat = new Mat();
+            CvInvoke.BilateralFilter(gray, bilat, 9, 50, 50);
+
+            // Laplacian filter
+            _lap = new Mat();
+            CvInvoke.Laplacian(bilat, _lap, DepthType.Cv64F, 3);
+
+            // Compute variance of the laplacian operator
+            var mean = new MCvScalar();
+            var stdDev = new MCvScalar();
+            CvInvoke.MeanStdDev(_lap, ref mean, ref stdDev);
+            var variance = stdDev.V0 * stdDev.V0;
+
+            return variance;
+        }
+
+        private void DisplayResults(double variance)
+        {
+            if (variance >= 100) label2.Text = "Not blurry";
+            else label2.Text = "Blurry";
+            label4.Text = $"{(int)variance}";
+        }
+
+        /// <summary>
+        /// Pre-processing step to adjust for varying degrees of image intensity
+        /// and enhance contrast.
+        /// </summary>
+        private Mat EqualizeHistogram(Mat src)
+        {
+            var labChannel = new Mat();
+            var hist = new Mat();
+
+            // READ RGB color image and convert it to Lab
+            CvInvoke.CvtColor(src, hist, ColorConversion.Bgr2Lab);
+
+            // Extract the L channel
+            CvInvoke.ExtractChannel(hist, labChannel, 0);
+
+            // apply the CLAHE algorithm to the L channel
+            CvInvoke.CLAHE(labChannel, 2, new Size(16, 16), labChannel);
+
+            // Merge the the color planes back into an Lab image
+            CvInvoke.InsertChannel(labChannel, hist, 0);
+
+            // convert back to RGB
+            CvInvoke.CvtColor(hist, hist, ColorConversion.Lab2Bgr);
+
+            labChannel.Dispose();
+
+            return hist;
+        }
+
+        /// <summary>
+        /// Computes bounds of scaled image within PictureBox.
+        /// </summary>
+        private (int top, int right, int bottom, int left) GetPictureBoxImageBounds(PictureBox pictureBox)
+        {
+            int top, right, bottom, left;
+
+            float pictureBoxAspectRatio = GetAspectRatio((float)pictureBox.Width, (float)pictureBox.Height);
+            float imgAspectRatio = GetAspectRatio((float)pictureBox.Image.Width, (float)pictureBox.Image.Height);
+
+            // PictureBox is wider or shorter than image. 
+            // Scale image to height of PictureBox and compute width by scale factor.
+            if (pictureBoxAspectRatio > imgAspectRatio)
+            {
+                float scaleFactor = (float)pictureBox.Height / (float)pictureBox.Image.Height;
+                float scaledWidth = (float)pictureBox.Image.Width * scaleFactor;
+                float padding = (pictureBox.Width - scaledWidth) / 2;
+
+                top = pictureBox.Top;
+                right = pictureBox.Right - (int)padding;
+                bottom = pictureBox.Bottom;
+                left = (int)padding;
+            }
+            // PictureBox is narrower or taller than image. 
+            // Scale image to width of PictureBox and compute height by scale factor.
+            else
+            {
+                float scaleFactor = (float)pictureBox.Width / (float)pictureBox.Image.Width;
+                float scaledHeight = (float)pictureBox.Image.Height * scaleFactor;
+                float padding = (pictureBox.Height - scaledHeight) / 2;
+
+                top = (int)padding;
+                right = pictureBox.Right;
+                bottom = pictureBox.Bottom - (int)padding;
+                left = pictureBox.Left;
+            }
+
+            return (top, right, bottom, left);
+        }
+
+        /// <summary>
+        /// Converts coordinates from PictureBox to source image.
+        /// </summary>
+        private Point ConvertCoordinates(PictureBox pictureBox, Point pictureBoxCoordinates)
+        {
+            int x, y;
+
+            float pictureBoxAspectRatio = GetAspectRatio((float)pictureBox.Width, (float)pictureBox.Height);
+            float imgAspectRatio = GetAspectRatio((float)pictureBox.Image.Width, (float)pictureBox.Image.Height);
+
+            if (pictureBoxAspectRatio > imgAspectRatio)
+            {
+                float scaleFactor = (float)pictureBox.Height / (float)pictureBox.Image.Height;
+
+                x = (int)((pictureBoxCoordinates.X - _imgBounds.left) / scaleFactor);
+                y = (int)(pictureBoxCoordinates.Y / scaleFactor);
+            }
+            else
+            {
+                float scaleFactor = (float)pictureBox.Width / (float)pictureBox.Image.Width;
+
+                x = (int)(pictureBoxCoordinates.X / scaleFactor);
+                y = (int)((pictureBoxCoordinates.Y - _imgBounds.top) / scaleFactor);
+            }
+
+            return new Point(x, y);
+        }
+
+        private float GetAspectRatio(float width, float height)
+        {
+            return width / height;
+        }
+
+        private bool ValidateCoordinates()
+        {
+            if (
+                _startPos.Y < _imgBounds.top ||
+                _startPos.Y > _imgBounds.bottom ||
+                _startPos.X < _imgBounds.left ||
+                _startPos.X > _imgBounds.right ||
+                _endPos.Y < _imgBounds.top ||
+                _endPos.Y > _imgBounds.bottom ||
+                _endPos.X < _imgBounds.left ||
+                _endPos.X > _imgBounds.right)
+                return false;
+
+            else return true;
+        }
+
+        private Rectangle GetRectangle(Point start, Point end)
+        {
+            return new Rectangle(
+                Math.Min(start.X, end.X),
+                Math.Min(start.Y, end.Y),
+                Math.Abs(start.X - end.X),
+                Math.Abs(start.Y - end.Y));
+        }
+
+        private void ShowLaplaceFilter(Mat input)
+        {
+            var output = new Mat();
+
+            CvInvoke.ConvertScaleAbs(input, output, 1, 0);
+
+            pictureBox1.Image = output.Bitmap;
+        }
+
+        private void showColorButton_Click(object sender, EventArgs e)
+        {
+            if (_img == null || _roi == null) return;
+            pictureBox1.Image = _roi.Bitmap;
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            if (_img == null) return;
+            pictureBox1.Image = _img.Bitmap;
+            label2.Text = "Please make selection";
+            label4.Text = "Please make selection";
+        }
+
+        private void showEdgesButton_Click(object sender, EventArgs e)
+        {
+            if (_img == null || _lap == null) return;
+            ShowLaplaceFilter(_lap);
         }
     }
 }
